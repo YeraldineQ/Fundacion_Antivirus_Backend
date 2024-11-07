674dca3209c5413dd1482a030740df07ca8cfac4
YERALDINE QUINTERO <yquintero411@misena.edu.co>
2024-11-07 15:07:39 -0500

    Avance de proyecto con User, Role, configSegurity, Token

---
+++ src/main/java/com/fundacionantivirus/backend/config/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.fundacionantivirus.backend.config;
+
+
+import com.fundacionantivirus.backend.jwt.JwtAuthenticationFilter;
+import com.fundacionantivirus.backend.jwt.JwtTokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+public class SecurityConfig {
+    private final JwtTokenProvider jwtTokenProvider;
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
+
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenProvider);
+        http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() //permite tener acceso swagger y docmentación
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/visitante/**").permitAll()
+                        .requestMatchers("/api/usuario/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .csrf(csrf -> csrf.disable())
+
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+
+    public AuthenticationManager
+    authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200")); //Permite solicitudes desde localhost:4200
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE",
+                "OPTIONS")); // Métodos permitidos
+        config.setAllowedHeaders(Arrays.asList("Authorization",
+                "Content-Type"));
+        config.setAllowCredentials(true); // Permitir credenciales (opcional)
+        UrlBasedCorsConfigurationSource source = new
+                UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // Aplica la configuración de CORS a todas las rutas
+        return new CorsFilter(source);
+    }
+}
---
+++ src/main/java/com/fundacionantivirus/backend/service/UserService.java
@@ -0,0 +1,82 @@
+package com.fundacionantivirus.backend.service;
+
+
+import com.fundacionantivirus.backend.exception.UserAlreadyExistsException;
+import com.fundacionantivirus.backend.model.Role;
+import com.fundacionantivirus.backend.model.User;
+import com.fundacionantivirus.backend.repository.RoleRepository;
+import com.fundacionantivirus.backend.repository.UserRepository;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private static Logger logger = (Logger)
+
+            LoggerFactory.getLogger(UserService.class);
+    @Autowired
+    private UserRepository userRepository;
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Autowired
+    private RoleRepository roleRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws
+
+            UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            logger.info(() -> "User not found");
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                getAuthorities(user) // Ensure this method correctly returns authorities
+        );
+    }
+    private Collection<? extends GrantedAuthority> getAuthorities(User user) {
+        logger.info(user::toString);
+// Implement authority retrieval logic (roles)
+        return user.getRoles().stream()
+
+                .map(role -> new SimpleGrantedAuthority("ROLE_"+
+
+                        role.getName()))
+
+                .collect(Collectors.toList());
+
+    }
+    // Optional: Method to register a new user
+    public void registerUser(User user) {
+// Verifica si el usuario ya existe por correo electrónico
+        if (userRepository.findByEmail(user.getEmail()) != null) {
+            throw new UserAlreadyExistsException("User with email " + user.getEmail() + " already exists.");
+        }
+        user.setPassword(passwordEncoder().encode(user.getPassword())); // Encode password
+// Define roles for the user
+        Set<Role> roles = new HashSet<>();
+        roles.add(roleRepository.findByName("USER")); // Asegúrate de que este rol exista en la base de datos
+        user.setRoles(roles);
+// Guarda el usuario en la base de datos
+        userRepository.save(user);
+    }
+}
